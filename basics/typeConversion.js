c6e6b12388c4d91778669ad2f3feb9d89066ba46
Puneet Kumar <164636741+The-Puneet-Kumar@users.noreply.github.com>
2024-05-25 12:44:22 +0000

    Conversion of datatype

---
+++ basics/typeConversion.js
@@ -0,0 +1,19 @@
+let score ="33";
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let string = Number(score)
+//console.log(typeof string);
+
+let isLoggedIn = "puneet"
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+
+// 1 => true; 0=> false
+// ''=> false;
+//"puneet"=>true
+
+let someNumber =33
+
+let stringNumber = String(someNumber)
+console.log(typeof stringNumber);
