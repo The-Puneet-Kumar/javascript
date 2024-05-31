09fe0b138215811e1b492056d27279ee842b65c2
Puneet Kumar <164636741+The-Puneet-Kumar@users.noreply.github.com>
2024-05-31 07:31:58 +0000

    scope

--- basics_03/scope.js
+++ basics_03/scope.js
@@ -1,14 +1,58 @@
-var c =300;
-let a =300;
-
-if(true){
-    let a =10;
-    const b=20;
-    //  var c= 30// output hamesa 30 ayega user confused 
-    console.log("Inner:",a)
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
 }
 
-console.log(a);
 
+
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++ function ese bhi declare hote h
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
